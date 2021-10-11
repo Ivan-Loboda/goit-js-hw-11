b8da2eb6ade80f60c51046931736bf27e65a8ce0
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-10-11 21:02:19 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,44 +2,86 @@ import './sass/main.scss';
 
 import { debounce } from 'lodash';
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 import { fetchImages } from './js/fetchImages'
 
 const inputForm = document.querySelector('#search-form');
+// const inputData = document.querySelector('#search-form input');
+const inputData = inputForm.elements['searchQuery'];
+const galleryList = document.querySelector('.gallery')
+const loadMoreBtn = document.querySelector('.load-more')
 
+let page = 1;
 
+function clearCards() {
+    galleryList.innerHTML = '';
+}
 
-inputForm.addEventListener('submit', debounce(getData, 300));
-
-
-
-
-
-
-
-function getData(e) {
-    e.prevent.default();
-    const inputData = inputForm.value.trim();
-    // countryCard.innerHTML = '';
-    // countryList.innerHTML = '';
-    // clearCards();
-
-    if (!inputData) {
+const getData = async () => {
+    if (!inputData.value) {
         return;
     };
-    fetchImages(inputData)
-        .then((data) => {
-            console.log(data)
-            if (data.status === 404) {
-                return Notiflix.Notify.info("Oops, there is no country with that name.")
-            };
-            if (data.length > 10) {
-                return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-            };
-            // if (data.length === 1) {
-            //     return printOne(data)
-            // };
-            // return printMany(data);
+    // console.log(inputData.value)
+
+    await fetchImages(inputData.value, page)
+        .then(response => {
+            const parsedData = response.data.hits;
+            console.log(parsedData)
+            if (parsedData.length === 0) {
+                Notiflix.Notify.failure(
+                    'Sorry, there are no images matching your search query. Please try again.',
+                );
+            }
+            else printImg(parsedData);
         })
-        .catch((error) => console.error(error))
-};
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+    });
+
+}
+
+inputForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    clearCards();
+    await getData();
+});
+
+
+function printImg(data) {
+    const markUp = data
+        .map(item => {
+            const { tags, likes, views, comments, downloads, webformatURL, largeImageURL } = item;
+            return `<div class="photo-card">
+                        <a href=${largeImageURL}>
+                        <img src="${webformatURL}" alt="${tags}" loading="lazy" width='200'/></a>
+                        <div class="info">
+                                <p class="info-item">
+                                    <b>Likes:</b>${likes}
+                                </p>
+                                <p class="info-item">
+                                    <b>Views:</b>${views}
+                                </p>
+                                <p class="info-item">
+                                    <b>Comments:</b>${comments}
+                                </p>
+                                <p class="info-item">
+                                    <b>Downloads:</b>${downloads}
+                                </p>
+                            </div>
+                        </div>`;
+        })
+        .join('');
+    galleryList.insertAdjacentHTML('beforeend', markUp);
+}
+
+
+loadMoreBtn.addEventListener('click', async e => {
+
+    page += 1;
+    await getData();
+    console.log(page);
+});
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,13 +1,21 @@
-const baseUrl = 'https://pixabay.com/api/?key=23763400-56e03b3c97aa031975e4c0255';
-const filterUrl = '&image_type=photo&orientation=horizontal&safesearch=true';
-const filterData = '&webformatURL&largeImageURL&tags&likes&views&comments&downloads'
+import axios from "axios";
+import Notiflix from 'notiflix';
 
-export const fetchImages = (inputData) => {
-    return fetch(`${baseUrl}&q${inputData}${filterUrl}${filterData}`)
-        .then(response => {
-            if (response.status >= 200 && response.status < 300) {
-                return response.json();
-            }
-            throw new Error(response.status);
-        })
+const baseUrl = 'https://pixabay.com/api/?key=23763400-56e03b3c97aa031975e4c0255';
+// const key = 'key=23763400-56e03b3c97aa031975e4c0255';
+const filterUrl = '&image_type=photo&orientation=horizontal&safesearch=true';
+
+
+export const fetchImages = async (inputData, page) => {
+    const data = await axios.get(`${baseUrl}&q=${inputData}${filterUrl}&page=${page}&per_page=40`)
+
+    if (data.status >= 200 && data.status < 300) {
+        return data;
+    }
+    if (data.status === 404) {
+        return Notiflix.Notify.info("Oops, there is no country with that name.")
+    };
+    throw new Error(data.status);
+
 };
+
